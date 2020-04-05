76b54a25624ba968e3a111e73f37e1f18831a426
ilievskif <filip.ilievski96@gmail.com>
2020-04-05 12:04:18 +0200

    Updates

---
+++ precache-manifest.7a2ddc2747938d738f4db3a842ea394c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d8800af614fa87617f5ff93c0421ec",
+    "url": "/BalkanWeatherApp/index.html"
+  },
+  {
+    "revision": "ee1459de2aea1dda7cd1",
+    "url": "/BalkanWeatherApp/static/css/main.8afdbeaf.chunk.css"
+  },
+  {
+    "revision": "b76dc6b739ad0c78d73c",
+    "url": "/BalkanWeatherApp/static/js/2.04455208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BalkanWeatherApp/static/js/2.04455208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1459de2aea1dda7cd1",
+    "url": "/BalkanWeatherApp/static/js/main.e4c2d2e4.chunk.js"
+  },
+  {
+    "revision": "b91055669aff1d4f2a7f",
+    "url": "/BalkanWeatherApp/static/js/runtime-main.4a7a3615.js"
+  },
+  {
+    "revision": "18c8c580b8a0b2b7a7e392c5a20ada52",
+    "url": "/BalkanWeatherApp/static/media/SairaCondensed-Regular.18c8c580.ttf"
+  },
+  {
+    "revision": "e1d7dd22b61a546b57b84d3f5842f4a1",
+    "url": "/BalkanWeatherApp/static/media/checkmark.e1d7dd22.svg"
+  },
+  {
+    "revision": "45c2c365b8aec104f11e3c3c8e5bbfa3",
+    "url": "/BalkanWeatherApp/static/media/options.45c2c365.svg"
+  }
+]);
